66c152616cd6feea2ad6fe2a7640f5144ffd701e
Feng Ni <fengni@huawei.com>
2020-04-15 11:14:35 +0800

    qemu: add pointer check in qemuMonitorLastError
    We found a exception when libvirt occurrs segmentation fault.
    thread 1 is waiting object lock in qemuConnectMonitor,
    qemu process exits and sends EOF event as well, so thread 2 invokes
    qemuMonitorLastError but pointer mon is NULL.
    
    Signed-off-by: Feng Ni <fengni@huawei.com>
    Signed-off-by: Xu Yandong <xuyandong2@huawei.com>

--- src/qemu/qemu_monitor.c
+++ src/qemu/qemu_monitor.c
@@ -966,7 +966,7 @@ qemuMonitorSend(qemuMonitorPtr mon,
 virErrorPtr
 qemuMonitorLastError(qemuMonitorPtr mon)
 {
-    if (mon->lastError.code == VIR_ERR_OK)
+    if (!mon || mon->lastError.code == VIR_ERR_OK)
         return NULL;
 
     return virErrorCopyNew(&mon->lastError);
