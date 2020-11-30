717b22f34cf5ce58be5a5223a29546e8c46e0365
Peter Krempa <pkrempa@redhat.com>
2020-11-30 15:32:14 +0100

    qemuMonitorJSONAddObject: Take double pointer for @props
    Prepare for a refactor of qemuMonitorJSONMakeCommandInternal.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- src/qemu/qemu_monitor_json.h
+++ src/qemu/qemu_monitor_json.h
@@ -234,7 +234,7 @@ int qemuMonitorJSONDelDevice(qemuMonitorPtr mon,
                              const char *devalias);
 
 int qemuMonitorJSONAddObject(qemuMonitorPtr mon,
-                             virJSONValuePtr props);
+                             virJSONValuePtr *props);
 
 int qemuMonitorJSONDelObject(qemuMonitorPtr mon,
                              const char *objalias,
