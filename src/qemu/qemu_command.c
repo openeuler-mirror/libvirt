ef8f291f6580427742c8068fae76dcfaec7da523
Matthew R. Ochs <mochs@nvidia.com>
2025-05-15 07:48:47 -0700

    qemu: Add command line support for PCI high memory MMIO size
    Add support for generating QEMU command line with PCI high memory MMIO size:
        - Add highmem-mmio-size to machine command line generation using
          size conveyed through pcihole64
        - Add validation for aarch64/virt machine type requirement
        - Add capability check for QEMU support
    
    This enables configuring the PCI high memory MMIO window size
    for aarch64 virt machine types using the existing pcihole64
    element.
    
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>
    
    # Conflicts:
    #	src/qemu/qemu_command.c

--- src/qemu/qemu_command.c
+++ src/qemu/qemu_command.c
@@ -6407,7 +6407,8 @@ qemuBuildGlobalControllerCommandLine(virCommand *cmd,
     for (i = 0; i < def->ncontrollers; i++) {
         virDomainControllerDef *cont = def->controllers[i];
         if (cont->type == VIR_DOMAIN_CONTROLLER_TYPE_PCI &&
-            cont->opts.pciopts.pcihole64) {
+            cont->opts.pciopts.pcihole64 &&
+            (qemuDomainIsQ35(def) || qemuDomainIsI440FX(def))) {
             const char *hoststr = NULL;
 
             switch (cont->model) {
@@ -7302,6 +7303,18 @@ qemuBuildMachineCommandLine(virCommand *cmd,
     }
 
     qemuBuildMachineACPI(&buf, def, qemuCaps);
+
+    if (qemuDomainIsARMVirt(def)) {
+        for (i = 0; i < def->ncontrollers; i++) {
+            virDomainControllerDef *cont = def->controllers[i];
+            if (cont->type == VIR_DOMAIN_CONTROLLER_TYPE_PCI &&
+                cont->opts.pciopts.pcihole64) {
+                virBufferAsprintf(&buf, ",highmem-mmio-size=%lluK", cont->opts.pciopts.pcihole64size);
+                break;
+            }
+        }
+    }
+
     virCommandAddArgBuffer(cmd, &buf);
 
     return 0;
